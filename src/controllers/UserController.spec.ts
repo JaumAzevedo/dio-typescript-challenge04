f8c8b91fca84530a2593148ae707baac0db1d163
João Paulo Azevedo <83193655+JaumAzevedo@users.noreply.github.com>
2023-01-19 13:33:57 -0400

    Add files via upload

---
+++ src/controllers/UserController.spec.ts
@@ -0,0 +1,65 @@
+import { UserController } from "./UserController";
+import { UserService } from '../services/UserService'
+import { Request } from 'express'
+import { makeMockResponse } from "../__mocks__/mockResponse.mock";
+
+describe('UserController', () => {
+    const mockUserService: Partial<UserService> = {
+        createUser: jest.fn(),
+        getAllUsers: jest.fn(),
+        deleteUser: jest.fn(),
+    }
+    
+    const userController = new UserController(mockUserService as UserService);
+
+    it('Deve adicionar um novo usuário', () => {
+        const mockRequest = {
+            body: {
+                name: 'Nath',
+                email: 'nath@test.com'
+            }
+        } as Request
+        const mockResponse = makeMockResponse()
+        userController.createUser(mockRequest, mockResponse)
+        expect(mockResponse.state.status).toBe(201)
+        expect(mockResponse.state.json).toMatchObject({ message: 'Usuário criado' })
+    })
+
+    it('Erro ao tentar inserir sem informar o nome', () => {
+        const mockRequest = {
+            body: {
+                email: 'jp@dio.com'
+            }
+        } as Request
+        const mockResponse = makeMockResponse()
+        userController.createUser(mockRequest, mockResponse)
+        expect(mockResponse.state.status).toBe(400)
+        expect(mockResponse.state.json).toMatchObject({ message: 'Bad request! Informar o email por favor' })
+    })
+
+    it('Service getAllUsers chamado', () => {
+        const mockRequest = {
+            body: {
+                name: 'Joao Paulo',
+                email: 'jp@dio.com'
+            }
+        } as Request
+        const mockResponse = makeMockResponse()
+        userController.getAllUsers(mockRequest, mockResponse)
+        expect(mockUserService.getAllUsers).toBeCalled()
+        expect(mockResponse.state.status).toBe(200)
+    })
+
+    it('Deve deletar um usuário', () => {
+        const mockRequest = {
+            body: {
+                name: 'Nath',
+                email: 'nath@test.com'
+            }
+        } as Request
+        const mockResponse = makeMockResponse()
+        userController.deleteUser(mockRequest, mockResponse)
+        expect(mockResponse.state.status).toBe(200)
+        expect(mockResponse.state.json).toMatchObject({ message: 'Usuário deletado' })
+    })
+})
